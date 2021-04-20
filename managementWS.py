27bd14c2b0b8a8b382f6c6bf6c925ea2433606ff
cyfinfaza <42043351+cyfinfaza@users.noreply.github.com>
2021-04-20 08:37:10 -0400

    running standalone code with different types than int

--- managementWS.py
+++ managementWS.py
@@ -216,7 +216,7 @@ def generateRequestBinary(job: EC_JobManager.Job):
     request.id = job.id
     request.inputMethod = job.meta['code']
     request.inputMethodName = "solution" if job.meta['editorType'] == "challenge" else job.meta['runMethod']
-    request.solutionMethod = job.meta['solution'] if 'solution' in job.meta else GENERIC_SOLUTION
+    request.solutionMethod = job.meta['solution'] if 'solution' in job.meta else job.meta['code'].replace(job.meta['runMethod'], "solution")
     request.inputs.extend(job.meta['args'])
     request.timeout = job.meta['timeout']
     return request.SerializeToString()
@@ -386,7 +386,7 @@ async def server(websocket: WebSocketServerProtocol, path):
             async for message in websocket:
                 response = reqres_pb2.Response()
                 response.ParseFromString(message)
-                # print(str(response))
+                print(str(response))
                 job: EC_JobManager.Job = jobManager.jobsByID()[response.id]
                 # print(job.meta)
                 # resultsMerged = []
